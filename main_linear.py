f967e2da7253b90948e4ce58e50f54be63ab56c1
videorighter <videorighter@ds.seoultech.ac.kr>
2021-12-05 17:30:29 +0900

    modify: modify main code
    main 실행 코드 수정

--- main_linear.py
+++ main_linear.py
@@ -3,18 +3,24 @@
 선형 모델 비교 코드
 '''
 
-
 import preprocessing
 from dataset import new_dataset
 from model import linear_regression, svr
 import time
 
-preprocessing.preprocessing_overlap(1, './label/labeling.csv', f'preprocessed/')
-preprocessing.preprocessing_overlap(2, './label/labeling.csv', f'preprocessed/')
-new_dataset.new_dataset("./dataset/mean_dataset.csv")
-new_dataset.condition_indicators_dataset_five()
 
-start = time.time()
-linear_regression.linear_regression_mean("./dataset/mean_dataset.csv")
-svr.li_svr_five("./dataset/condition_indicators_dataset_five.csv")
-print(f"time: {time.time() - start}")
+def main():
+
+    preprocessing.preprocessing_overlap(1, './label/labeling.csv', f'preprocessed/')
+    preprocessing.preprocessing_overlap(2, './label/labeling.csv', f'preprocessed/')
+    new_dataset.new_dataset("./dataset/mean_dataset.csv")
+    new_dataset.condition_indicators_dataset_five()
+
+    start = time.time()
+    linear_regression.linear_regression_mean("./dataset/mean_dataset.csv")
+    svr.li_svr_five("./dataset/condition_indicators_dataset_five.csv")
+    print(f"time: {time.time() - start}")
+
+
+if __name__ == "__main__":
+    main()
